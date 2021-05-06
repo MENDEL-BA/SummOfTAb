b66da009258125ffb8d009fb365c4b41cd75c36a
MendelBa <>
2021-05-06 18:21:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function simpleArraySum(ar) {
+    // Write your code here
+    
+    const reducer = (accumulator, currentValue)  => accumulator + currentValue;
+    
+    return ar.reduce(reducer);
+
+}
